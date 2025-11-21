10c9d5cca9b3bbcd23ff106f1038b9355c14b81c
dinopatates <gustavecheval819@gmail.com>
2025-11-21 15:42:34 +0100

    lint test

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,9 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-undef": 'warn'
+
+  },
   },
 ])
--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Button from './components/button/Button.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
-
+console.log("App component rendered")
   return (
     <>
       <div>
